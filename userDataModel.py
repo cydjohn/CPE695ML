4479f5641745f46b834dc151bbd40f0f95284fa8
cydjohn <cydjohn@live.com>
2016-10-17 11:17:55 -0400

    update userDataModel

--- userDataModel.py
+++ userDataModel.py
@@ -10,14 +10,14 @@ class userDataModel(object):
 	def insertUserDataIntoDatabase(self,data):
 		if userdb.find_one(data["id_str"]) is False:
 			userdb.addData(data)
-			print 'new user added :' + data["id_str"]
+			print 'new user added id:' + data["id_str"]
 
 	def deleteUselessUserById(self):
 		for d in userdb.getAllData():
 			# print d["user"]["id_str"]
 			if not twitterdb.findUserById(d["id_str"]):
 				userdb.deleteUserById(d["id_str"])
-				print 'delete a user :' + d["id_str"]
+				print 'delete user :' + d["id_str"]
 			
 		# 		userdb.deleteUserById(d["id_str"])
 		# print 'lal'
